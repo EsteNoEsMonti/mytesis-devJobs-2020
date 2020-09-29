7db52e98f40777a775dab4f94add4bc4031476f0
estenoesmonti <estenoesmonti@gmail.com>
2020-09-29 18:54:49 -0300

    TICKET-02 SUB-02 Creando Formulario para vacantes

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-const router = require('./routes')
+const router = require('./routes');
+
+require('dotenv').config({ path : 'variables.env'});
 
 const app = express();
 
@@ -18,6 +26,16 @@ app.set('view engine', 'handlebars');
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection : mongoose.connection })
+}));
+
 app.use('/', router())
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
+const vacantesController = require('../controllers/vacantesController');
+// const usuariosController = require('../controllers/usuariosController');
+// const authController = require('../controllers/authController');
 
 module.exports = () => {
   router.get('/', homeController.mostrarTrabajos);
+
+  // Crear Vacantes
+  router.get('/vacantes/nueva',
+    // authController.verificarUsuario,
+    vacantesController.formularioNuevaVacante
+  );
+  // router.post('/vacantes/nueva',
+  //   authController.verificarUsuario,
+  //   vacantesController.validarVacante,
+  //   vacantesController.agregarVacante
+  // );
 
   return router;
 }
